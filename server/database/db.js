cdabf0747a25f92e5df25d224a8162555c6f853c
Hitesh <hiteshgahanolia@gmail.com>
2023-03-31 14:09:54 +0530

    Connected To DataBase and Created Schema For the Login and SignUp Page

--- server/database/db.js
+++ server/database/db.js
@@ -1,16 +1,21 @@
+const mongoose = require('mongoose');
+const URL = `mongodb+srv://hitesh:12345@foodapp.jipqsyv.mongodb.net/MyFood?retryWrites=true&w=majority`;
 
-const mongoose = require('mongoose')
-
-const URL = `mongodb+srv://hitesh:12345@foodapp.jipqsyv.mongodb.net/?retryWrites=true&w=majority`;
 const Connection = async () => {
     try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log("Connected To Database Successfully");
 
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
-        console.log("Connected To Database SuccessFully");
+        const fetchedData = await mongoose.connection.db.collection("food_items").find({}).toArray();
+        // console.log("Fetched Data: ", fetchedData);
+        
+        // Return the fetched data or pass it to a callback
+        return fetchedData;
     }
     catch (error) {
-        console.log("Error While Connecting to DataBase", error.message);
+        console.log("Error While Connecting to Database", error.message);
+        // Handle the error appropriately
     }
-}
+};
 
 module.exports = Connection;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-
-
-const express=require('express')
-const Connection=require('./database/db')
-// dotenv.config();
+const express = require('express')
 const app = express();
 const PORT = 8000;
 
+const Connection = require('./database/db')
+
 Connection();
 
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+app.use(express.json())
+app.use('/api', require("./Routes/CreateUser"))
 
 app.listen(PORT, () => {
     console.log(`Server Started on ${PORT} `)
 })
-
